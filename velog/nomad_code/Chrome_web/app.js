4547d81f96fa8551be0c917774f6f83ddb22d978
joshua <iuttn123@gmail.com>
2021-11-23 20:52:48 +0900

    #4

--- velog/nomad_code/Chrome_web/app.js
+++ velog/nomad_code/Chrome_web/app.js
@@ -2,8 +2,18 @@ const loginInput = document.querySelector(`#login-form input`);
 const loginButton = document.querySelector(`#login-form button`);
 
 function handleLoginBtnClick(){
-    console.log(loginInput.value);
-    console.log(loginButton);
+    const userName = loginInput.value;
+    if(userName==="") alert("Please write your name.");
+    else if(userName.length > 15) alert("Your name is too long.");
+
+}
+loginButton.addEventListener("click",handleLoginBtnClick);
+
+const link = document.querySelector("a");
+
+function handleLinkClick(event) {
+    console.log(event)
+    console.dir(event)
 }
 
-loginButton.addEventListener("click",handleLoginBtnClick);
+link.addEventListener("click",handleLinkClick);
